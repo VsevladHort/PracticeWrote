059b11db8c890ce972f896271668e469fe4f978c
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-06-18 11:44:55 +0300

    change WroteDao to more accurately represent DAO pattern

--- app/src/main/java/com/dak/wrote/backend/abstractions/dao/WroteDao.kt
+++ app/src/main/java/com/dak/wrote/backend/abstractions/dao/WroteDao.kt
@@ -6,11 +6,13 @@ import com.dak.wrote.backend.abstractions.entities.BaseNote
 import com.dak.wrote.backend.abstractions.entities.UniqueEntity
 
 interface WroteDao {
-    suspend fun createBook(title: String): Book?
-    suspend fun createNote(parent: UniqueEntity, title: String, type: NoteType): BaseNote?
+    suspend fun insertBook(title: String): Book?
+    suspend fun insetNote(parent: UniqueEntity, title: String, type: NoteType): BaseNote?
     suspend fun updateNoteEntry(note: BaseNote): Boolean
     suspend fun updateNoteObject(note: BaseNote): Boolean
     suspend fun updateBookEntry(book: Book): Boolean
     suspend fun updateBookObject(book: Book): Boolean
     suspend fun getNote(uniqueKey: String): BaseNote?
+    suspend fun getBooks(uniqueKey: String): List<Book>?
+    suspend fun deleteEntity(entity: UniqueEntity): Boolean
 }
