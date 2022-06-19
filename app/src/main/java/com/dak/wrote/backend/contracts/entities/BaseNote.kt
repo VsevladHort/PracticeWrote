714c9269d482a4314f2a96564f927a2aaaf08be6
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2022-06-19 16:58:11 +0300

    update BaseNote interface to require attrs and alt titles

--- app/src/main/java/com/dak/wrote/backend/contracts/entities/BaseNote.kt
+++ app/src/main/java/com/dak/wrote/backend/contracts/entities/BaseNote.kt
@@ -4,6 +4,8 @@ import com.dak.wrote.backend.contracts.entities.constants.NoteType
 
 interface BaseNote : SearchableEntity, UniqueEntity {
     var title: String
+    var alternateTitles: Set<String>
+    var attributes: Set<Attribute>
     val type: NoteType
     fun generateSaveData(): ByteArray
     fun loadSaveData(value: ByteArray)
