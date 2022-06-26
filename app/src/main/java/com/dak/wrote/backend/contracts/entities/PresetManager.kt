c9b9865a33ae73e7bf936833dac2e417ccbfdaf0
Andrii Zahorulko <and.zagorulko@gmail.com>
2022-06-26 20:21:34 +0300

    PresetManager

---
+++ app/src/main/java/com/dak/wrote/backend/contracts/entities/PresetManager.kt
@@ -0,0 +1,8 @@
+package com.dak.wrote.backend.contracts.entities
+
+interface PresetManager<Display, Full> {
+    fun loadDisplay(byteData : ByteArray) : Display
+    fun loadFull(byteData: ByteArray) : Full
+    fun saveDisplay(display : Display) : ByteArray
+    fun saveFull(full: Full) : ByteArray
+}
